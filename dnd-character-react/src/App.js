bbddc7f21f571ad9475068ec1a5980b489476827
Anna Utkin <annau25@gmail.com>
2022-11-19 13:25:41 -0500

    Add new pages

--- dnd-character-react/src/App.js
+++ dnd-character-react/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <nav>
+      <h1 style={{ textAlign:'center' }}>DND Character Build</h1>
+        <ul>
+          <li>Home</li>
+          <li>Create Character</li>
+          <li>My Characters</li>
+        </ul>
+      </nav>
     </div>
   );
 }
